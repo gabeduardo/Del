54a0cadea3db40741229642cf6bec7836b1e2d8d
gabriel <gemcir13@gmail.com>
2020-11-22 14:04:29 -0400

    agregando ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from 'react'
+import Layout from './src/components/Layout'
+import { OrderProvider } from './src/components/OrderContext'
 
-// You can delete this file if you're not using it
+export function wrapPageElement({ element, props }) {
+  return <Layout {...props}> {element} </Layout>
+}
+
+export function wrapRootElement({ element }) {
+  return <OrderProvider>{element}</OrderProvider>
+}
